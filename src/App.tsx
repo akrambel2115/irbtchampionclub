a00062901b4e30dd628c2f639b325ecb7560db93
Belbakhouche Akram <akram.khaled.belbakhouche@ensia.edu.dz>
2025-07-26 03:24:53 +0100

    add privacy and conditions pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsAndConditions from "./pages/TermsAndConditions";
 import ClickSpark from "@/blocks/Animations/ClickSpark/ClickSpark";
 
 const queryClient = new QueryClient();
@@ -35,6 +37,8 @@ const App = () => {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Index />} />
+              <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+              <Route path="/TermsAndConditions" element={<TermsAndConditions />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
